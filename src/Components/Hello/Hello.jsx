8db968f9286aba4b3bfdd3fcd9a5b66982e78305
Kryštof Svejkovský <svejkovsky.kry@gmail.com>
2024-02-08 20:27:30 +0100

    yeye

--- src/Components/Hello/Hello.jsx
+++ src/Components/Hello/Hello.jsx
@@ -1,29 +1,36 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { ThemeContext } from '../../context/ThemeContext';
 import './Hello.scss';
 
 function Hello() {
     const { theme } = useContext(ThemeContext);
+    const [isTransformed, setIsTransformed] = useState(false);
 
-    const divStyle = {
-        backgroundImage: "url('/img/background/background.gif')",
-        backgroundPosition: 'center',
-        backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat',
-    };
+    const toggleTransformation = () => setIsTransformed(!isTransformed);
 
-    const textContent = theme === 'light' ? 
-        { welcomeText: "Welcome to the Light Theme", buttonText: "Light Mode" } : 
-        { welcomeText: "Welcome to the Dark Theme", buttonText: "Dark Mode" };
+    const logoImagePath = process.env.PUBLIC_URL + '/img/logo.png';
+    const lightProfileImagePath = process.env.PUBLIC_URL + '/img/profile/light.jpeg';
+    const darkProfileImagePath = process.env.PUBLIC_URL + '/img/profile/dark.jpeg';
+
+    const imageToShow = isTransformed
+        ? theme === 'light' ? lightProfileImagePath : darkProfileImagePath
+        : logoImagePath;
 
     return (
-        <div className={`hello ${theme}`} style={divStyle}>
-            <div className="center-image-container">
-                <img src="/img/logo.png" alt="Center" />
+        <div className={`hello ${theme} ${isTransformed ? 'transformed' : ''}`}>
+            <div className="background-layer">
+                <img src={`${process.env.PUBLIC_URL}/img/background/background.gif`} alt="" />
             </div>
-            <div className="text-and-button-container">
-                <p>{textContent.welcomeText}</p>
-                <button>{textContent.buttonText}</button>
+
+            <div className="content">
+                <div className="logo-container">
+                    <img src={imageToShow} alt="Display" />
+                </div>
+                <div className="text-and-button-container">
+                    <p>{isTransformed ? "About Me" : theme === 'light' ? "Welcome to the Light Theme" : "Welcome to the Dark Theme"}</p>
+                    <button onClick={toggleTransformation}>{isTransformed ? "Go Back" : "Transform Background"}</button>
+                    {isTransformed && <button>Another Action</button>}
+                </div>
             </div>
         </div>
     );
